5c4b1844cd5169c22c6af1094c31c867450b5b22
samfurr <samneillfurr@gmail.com>
2020-12-12 14:50:01 -0500

    add header and footer

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import { BrowserRouter as Router, Route, NavLink } from 'react-router-dom';
 import NavBar from './Components/Navbar/Navbar';
+import Footer from './Components/Footer/Footer';
 import Locations from './Components/Locations';
 import Events from './Components/Events';
 import ToDo from './Components/ToDo';
@@ -10,14 +11,15 @@ import HomePage from './Components/HomePage';
 function App() {
   return (
     <div className="App">
-      <h1>Whats Happening</h1>
-      <br/>
       <Router>
         <NavBar />
-        <Route exact path="/" component={HomePage} />
-        <Route exact path="/locations" component={Locations} />
-        <Route exact path="/events" component={Events} />
-        <Route exact path="/about" component={ToDo} />
+        <div className="main-content">
+            <Route exact path="/" component={HomePage} />
+            <Route exact path="/locations" component={Locations} />
+            <Route exact path="/events" component={Events} />
+            <Route exact path="/about" component={ToDo} />
+        </div>
+        <Footer />
       </Router>
     </div>
   );
