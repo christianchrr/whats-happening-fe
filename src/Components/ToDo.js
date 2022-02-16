011f418a9cb741615db91a76f1c1bf5659b25ee5
christianchrr <41273785+christianchrr@users.noreply.github.com>
2022-02-16 13:38:18 -0500

    fix

--- src/Components/ToDo.js
+++ src/Components/ToDo.js
@@ -7,13 +7,9 @@ const ToDo = () => {
             <h3>To Do</h3>
             <br/>
             <ul>
-                <li>Finish pages so that relationship looks cleaner</li>
                 <li>Add Login</li>
                 <li>Add more CSS</li>
                 <li>Add Home Page</li>
-                <li>Add limitations to forms</li>
-                <li>Get date form field working</li>
-                <li>fix dropdown reset on form submit</li>
             </ul>
         </div>
     )
